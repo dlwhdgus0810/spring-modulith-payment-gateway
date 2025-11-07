41378e54a0f2d520270ec81cd3079493169611ee
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2025-11-07 11:42:24 -0600

    refactor: Consolidate user commands into a sealed class to simplify structure and validation

---
+++ src/main/kotlin/me/hyunlee/laundry/user/domain/port/in/UserCommand.kt
@@ -0,0 +1,31 @@
+package me.hyunlee.laundry.user.domain.port.`in`
+
+import me.hyunlee.laundry.common.UserId
+import me.hyunlee.laundry.user.domain.model.Address
+
+sealed class UserCommand
+
+data class AddAddressCommand(
+    val userId: UserId,
+    val address: Address
+)
+
+data class RegisterUserCommand(
+    val phone: String,
+    val email: String?,
+    val firstName: String,
+    val lastName: String,
+) {
+    init {
+        require(phone.isNotBlank()) { "Phone number cannot be blank" }
+        require(firstName.isNotBlank()) { "First name cannot be blank" }
+        require(lastName.isNotBlank()) { "Last name cannot be blank" }
+    }
+}
+
+data class UpdateUserProfileCommand(
+    val userId: UserId,
+    val email: String?,
+    val firstName: String?,
+    val lastName: String?
+)
--- src/main/kotlin/me/hyunlee/laundry/user/domain/port/in/command/RegisterUserCommand.kt
+++
@@ -1,9 +0,0 @@
-package me.hyunlee.user.domain.port.inbound.commands
-
-data class RegisterUserCommand(
-    val phone: String,
-    val email: String?,
-    val firstName: String,
-    val lastName: String,
-) {
-}
