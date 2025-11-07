32d90717c172e6437a15493019e2913eb72efdbf
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2025-11-07 11:48:48 -0600

    feat: Add UserMapper to convert between User and UserEntity models

---
+++ src/main/kotlin/me/hyunlee/laundry/user/adapter/out/persistence/user/UserMapper.kt
@@ -0,0 +1,57 @@
+package me.hyunlee.laundry.user.adapter.out.persistence.user
+
+import me.hyunlee.laundry.common.UserId
+import me.hyunlee.laundry.user.domain.model.Address
+import me.hyunlee.laundry.user.domain.model.User
+
+fun UserEntity.toDomain() : User {
+    return User(
+        id = UserId(id),
+        phone = phone,
+        email = email,
+        firstName = firstName,
+        lastName = lastName,
+        addresses = addresses.map { it.toDomain() },
+        customerId = customerId
+    )
+}
+
+fun User.toEntity() : UserEntity {
+    val ue = UserEntity(
+        id = id.value,
+        phone = phone,
+        email = email,
+        firstName = firstName,
+        lastName = lastName,
+        customerId = customerId
+    )
+
+    ue.addresses = addresses.map { it.toEntity().apply { user = ue } }.toMutableList()
+
+    return ue
+}
+
+fun AddressEntity.toDomain() : Address {
+    return Address(
+        id = id,
+        street = street,
+        city = city,
+        state = state,
+        postalCode = postalCode,
+        secondary = secondary,
+        instructions = instructions,
+        isPrimary = isPrimary
+    )
+}
+
+fun Address.toEntity() : AddressEntity {
+    return AddressEntity(
+        street = street,
+        city = city,
+        state = state,
+        postalCode = postalCode,
+        secondary = secondary,
+        instructions = instructions,
+        isPrimary = isPrimary,
+    )
+}
