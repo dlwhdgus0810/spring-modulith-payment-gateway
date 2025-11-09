d2d1bc2524aeb4b1d824f41b3f19ae42d0f4f158
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2025-11-09 00:34:59 -0600

    feat: Add modular boundary verification to LaundryApplicationTests
    Replaced the `contextLoads()` test with `verify modular boundaries()` to ensure the application respects defined modular boundaries. Introduced `ApplicationModules` to check module integrity through the `verify()` method. This change enhances the test suite by validating module structure adherence.

--- src/test/kotlin/me/hyunlee/laundry/LaundryApplicationTests.kt
+++ src/test/kotlin/me/hyunlee/laundry/LaundryApplicationTests.kt
@@ -2,12 +2,15 @@ package me.hyunlee.laundry
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.modulith.core.ApplicationModules
 
 @SpringBootTest
 class LaundryApplicationTests {
 
     @Test
-    fun contextLoads() {
+    fun `verify modular boundaries`() {
+        val modules = ApplicationModules.of(Application::class.java)
+        modules.verify()
     }
 
 }
