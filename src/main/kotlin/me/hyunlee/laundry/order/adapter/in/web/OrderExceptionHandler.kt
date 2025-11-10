721e0aa3883f2af8cdcc74a4aeb0715b7414f6b9
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2025-11-10 02:48:29 -0600

    feat: Add OrderExceptionHandler to manage order-related exceptions

---
+++ src/main/kotlin/me/hyunlee/laundry/order/adapter/in/web/OrderExceptionHandler.kt
@@ -0,0 +1,51 @@
+package me.hyunlee.laundry.order.adapter.`in`.web
+
+import me.hyunlee.laundry.common.adapter.ApiResponse
+import me.hyunlee.laundry.order.domain.exception.OrderException
+import org.slf4j.LoggerFactory
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice(basePackages = ["me.hyunlee.laundry.order"])
+@Component("orderGlobalExceptionHandler")
+class OrderExceptionHandler {
+
+    private val logger = LoggerFactory.getLogger(OrderExceptionHandler::class.java)
+
+    @ExceptionHandler(OrderException.UserNotFoundException::class)
+    fun handle(e: OrderException.UserNotFoundException, req: WebRequest): ResponseEntity<ApiResponse<Nothing>> {
+        logger.warn("[ORDER] user not found: ${e.message}")
+
+        val response = ApiResponse.exceptionError<Nothing>(
+            msg = e.message ?: "User not found",
+            errCode = "USER_NOT_FOUND",
+            path = getPath(req)
+        )
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handle(e: Exception, req: WebRequest): ResponseEntity<ApiResponse<Nothing>> {
+        logger.warn("error occurred: ${e.message}")
+
+        val response = ApiResponse.exceptionError<Nothing>(
+            msg = e.message ?: "[ORDER] error occurred",
+            errCode = "SERVER_EXCEPTION",
+            path = getPath(req)
+        )
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response)
+    }
+
+    private fun getPath(req : WebRequest) : String? {
+        return req.getDescription(false).removePrefix("uri=").takeIf { it.isNotBlank() }
+    }
+}
