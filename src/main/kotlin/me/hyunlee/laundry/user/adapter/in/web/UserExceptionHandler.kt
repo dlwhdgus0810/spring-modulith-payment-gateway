79fce17dedec36ea7ad9b332ef00a993dd06e4e7
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2025-11-11 01:19:32 -0600

    refactor: Rename `GlobalExceptionHandler` to `UserExceptionHandler` and refactor for improved modularity
    - Updated package from `rest` to `web` for consistency.
    - Added `@Order` annotation to set precedence.
    - Introduced a generic exception handler for broader error handling.
    - Adjusted logger to include user-specific context.

---
+++ src/main/kotlin/me/hyunlee/laundry/user/adapter/in/web/UserExceptionHandler.kt
@@ -0,0 +1,51 @@
+package me.hyunlee.laundry.user.adapter.`in`.web
+
+import me.hyunlee.laundry.common.adapter.`in`.web.ApiResponse
+import me.hyunlee.laundry.user.domain.exception.UserException.UserNotFoundException
+import org.slf4j.LoggerFactory
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice(basePackages = ["me.hyunlee.laundry.user"])
+@Component("userGlobalExceptionHandler")
+class UserExceptionHandler {
+
+    private val logger = LoggerFactory.getLogger(UserExceptionHandler::class.java)
+
+    @ExceptionHandler(UserNotFoundException::class)
+    fun handle(e: UserNotFoundException, req: WebRequest): ResponseEntity<ApiResponse<Nothing>> {
+        logger.warn("[USER] user not found: ${e.message}")
+
+        val response = ApiResponse.exceptionError<Nothing>(
+            msg = e.message ?: "User not found",
+            errCode = "USER_NOT_FOUND",
+            path = getPath(req)
+        )
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handle(e: Exception, req: WebRequest): ResponseEntity<ApiResponse<Nothing>> {
+        logger.warn("error occurred: ${e.message}")
+
+        val response = ApiResponse.exceptionError<Nothing>(
+            msg = e.message ?: "[USER] error occurred",
+            errCode = "SERVER_EXCEPTION",
+            path = getPath(req)
+        )
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response)
+    }
+
+    private fun getPath(req : WebRequest) : String? {
+        return req.getDescription(false).removePrefix("uri=").takeIf { it.isNotBlank() }
+    }
+}
