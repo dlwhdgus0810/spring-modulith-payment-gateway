e7140e8d37cc984128b05d14b1c48a2b5e765ef8
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2025-11-11 11:55:54 -0600

    refactor: Remove `PaymentMethodCommand` and associated interfaces for domain simplification
    - Deleted `PaymentMethodCommand` and its concrete implementations (`CreateCardOrWalletCommand`, `CreateAchCommand`, etc.) to streamline domain logic.
    - Removed `PaymentMethodCommandUseCase`, `PaymentMethodQueryUseCase`, and `PaymentMethodRepository` interfaces to reduce complexity.
    - Simplified the structure by eliminating redundant or unused commands and repository methods.

--- src/main/kotlin/me/hyunlee/laundry/payment/domain/port/in/PaymentMethodCommandUseCase.kt
+++
@@ -1,7 +0,0 @@
-package me.hyunlee.laundry.payment.domain.port.`in`
-
-import me.hyunlee.laundry.payment.domain.model.PaymentMethod
-
-interface PaymentMethodCommandUseCase {
-    fun create(command : CreateCardOrWalletCommand) : PaymentMethod
-}
--- src/main/kotlin/me/hyunlee/laundry/payment/domain/port/in/PaymentMethodQueryUseCase.kt
+++
@@ -1,4 +0,0 @@
-package me.hyunlee.laundry.payment.domain.port.`in`
-
-interface PaymentMethodQueryUseCase {
-}
