d9fd920471280d126099623dc00dc0aacf4c38d5
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2025-11-17 10:49:25 -0600

    feat: Add `OrderNotFoundException` to `OrderException` hierarchy

--- src/main/kotlin/me/hyunlee/laundry/order/domain/exception/OrderException.kt
+++ src/main/kotlin/me/hyunlee/laundry/order/domain/exception/OrderException.kt
@@ -2,5 +2,6 @@ package me.hyunlee.laundry.order.domain.exception
 
 sealed class OrderException(message: String) : RuntimeException(message) {
     class UserNotFoundException(userId: String) : OrderException("User not found: $userId")
+    class OrderNotFoundException(orderId: String) : OrderException("Order not found, orderId: $orderId")
 }
 
