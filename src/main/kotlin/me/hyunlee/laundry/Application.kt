0382a90c06fe65d068d61129384714d2653575d1
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2025-11-22 21:22:02 -0600

    feat: Enable scheduling and JPA auditing in the application configuration
    - Added `@EnableScheduling` for scheduling support.
    - Added `@EnableJpaAuditing` for automatic auditing in JPA entities.

--- src/main/kotlin/me/hyunlee/laundry/Application.kt
+++ src/main/kotlin/me/hyunlee/laundry/Application.kt
@@ -2,9 +2,13 @@ package me.hyunlee.laundry
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.modulith.Modulithic
+import org.springframework.scheduling.annotation.EnableScheduling
 
-@Modulithic(sharedModules = ["me.hyunlee.laundry.common"])
+@Modulithic
+@EnableScheduling
+@EnableJpaAuditing
 @SpringBootApplication
 class Application
 
