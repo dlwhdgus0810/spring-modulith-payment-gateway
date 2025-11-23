4d62b1315a357dd5577948a1c5f7ae475bbe926c
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2025-11-22 21:20:18 -0600

    refactor: Allow `monitoring` dependency in `ModuleInfo` configuration

--- src/main/kotlin/me/hyunlee/laundry/payment/ModuleInfo.kt
+++ src/main/kotlin/me/hyunlee/laundry/payment/ModuleInfo.kt
@@ -1,4 +1,5 @@
 package me.hyunlee.laundry.payment
 
-@org.springframework.modulith.ApplicationModule(allowedDependencies = ["common :: adapter", "common :: domain"])
+@org.springframework.modulith.ApplicationModule(allowedDependencies =
+    ["common :: adapter", "common :: domain", "monitoring"])
 object ModuleInfo
